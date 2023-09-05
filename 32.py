b56b7afa5faafd71e9aa38d0c1e3b346d3dccd31
widerhin <widerhin@yandex.ru>
2023-09-05 19:58:59 +0300

    task 32

--- 32.py
+++ 32.py
@@ -12,7 +12,7 @@ def search_place(listos,min,max):
     place=[]
     for i in listos:
         if i<max and i>min :
-            place.append(listos.index(i))
+            place.append(listos.index(i))#через счётчик надёжнее, но не зря же этот метод есть
     return(place)
 
 print(search_place(create(int(input("размер массива "))),int(input("минимальное число")),int(input("максисальное число "))))
