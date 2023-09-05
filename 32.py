83a89a755a02f6f76b668040a4e9dcc013d26ca7
widerhin <widerhin@yandex.ru>
2023-09-05 19:57:24 +0300

    task 32

---
+++ 32.py
@@ -0,0 +1,18 @@
+import random as gluk
+
+def create(num):
+    listos=[]
+    for i in range(0,num):
+        tmp=gluk.randint(0,1000)
+        listos.append(tmp)
+    print(listos)
+    return(listos)
+
+def search_place(listos,min,max):
+    place=[]
+    for i in listos:
+        if i<max and i>min :
+            place.append(listos.index(i))
+    return(place)
+
+print(search_place(create(int(input("размер массива "))),int(input("минимальное число")),int(input("максисальное число "))))
